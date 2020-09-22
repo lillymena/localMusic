29990b7028ed7815aea9972d1beb1f5e284803a7
Lillyann Mena <lillyannmena.lm@gmail.com>
2020-09-22 18:18:20 -0400

    (done with lab 2)

--- app/routes.py
+++ app/routes.py
@@ -9,17 +9,23 @@ def homepage():
     body = {'body': 'come find out about my current favorite artists :)'}
     return render_template('base.html', title='home', user=title, body=body)
 
+
 @app.route('/artists')
 def listOfArtists():
     title = {'title': 'this is a list of my favorite artists'}
-    body = {'artist': 'SIR'},\
-           {'artist': 'Smino'},\
-           {'artist': 'SZA'}
+    body = {'artist': 'SIR'}, {'artist': 'Smino'}, {'artist': 'SZA'}
     return render_template('localArtist.html', title='home', user=title, body=body)
 
-# @app.route('/newArtist')
-# def newArtist():
-#     title = {'title': 'this is a list of new artists'}
-#     body = {'artist'}
-#     return render_template('newArtist.html', title='home', user=title, body=body)
 
+@app.route('/newArtist')
+def createNewArtist():
+    title = {'title': 'New Artists'}
+    body = {'artist'}
+    return render_template('newArtist.html', title='home', user=title, body=body)
+
+
+@app.route('/sir')
+def sir():
+    title = {'title': 'This is Sir'}
+    body = {''}
+    return render_template('sir.html', title='home', user=title, body=body)
