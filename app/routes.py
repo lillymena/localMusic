ec675db2776050b15d9d05f1c7b58456fdbc4bf4
Lillyann Mena <lillyannmena.lm@gmail.com>
2020-09-22 11:44:43 -0400

    updated routes/base + fixed some of the html

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,25 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+@app.route('/home')
+def homepage():
+    title = {'title': 'welcome to my local music database'}
+    body = {'body': 'come find out about my current favorite artists :)'}
+    return render_template('base.html', title='home', user=title, body=body)
+
+@app.route('/artists')
+def listOfArtists():
+    title = {'title': 'this is a list of my favorite artists'}
+    body = {'artist': 'SIR'},\
+           {'artist': 'Smino'},\
+           {'artist': 'SZA'}
+    return render_template('localArtist.html', title='home', user=title, body=body)
+
+# @app.route('/newArtist')
+# def newArtist():
+#     title = {'title': 'this is a list of new artists'}
+#     body = {'artist'}
+#     return render_template('newArtist.html', title='home', user=title, body=body)
+
